71092eeb60ae2e9ac8f558f8b5aa399793af7d02
Marcos-Gregorio <marcosgregs.frontend@gmail.com>
2023-03-20 12:04:55 -0300

    AddMyProjectSection2.

--- ReactPortWeb/src/assets/MyProjects/MyProjects.jsx
+++ ReactPortWeb/src/assets/MyProjects/MyProjects.jsx
@@ -57,7 +57,7 @@ export const MyProject =()=>{
             </div>
           
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
+ OneBitCode
 </button>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
